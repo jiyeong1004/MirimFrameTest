cef0dbdced4c8cbdb7afa105cdb2f494a77722be
jiyeong1004 <ak1157@naver.com>
2020-10-27 19:36:31 +0900

    Complete FrameLayoutTest

--- app/src/main/java/com/example/mirimframetest/MainActivity.java
+++ app/src/main/java/com/example/mirimframetest/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.mirimframetest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linearDog, linearCat, linearRabbit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linearDog = findViewById(R.id.linear_dog);
+        linearCat = findViewById(R.id.linear_cat);
+        linearRabbit = findViewById(R.id.linear_rabbit);
+
+        Button btnDog = findViewById(R.id.btn_dog);
+        Button btnCat = findViewById(R.id.btn_cat);
+        Button btnRabbit = findViewById(R.id.btn_rabbit);
+
+        btnDog.setOnClickListener(btnListener);
+        btnCat.setOnClickListener(btnListener);
+        btnRabbit.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            linearDog.setVisibility(View.INVISIBLE);
+            linearCat.setVisibility(View.INVISIBLE);
+            linearRabbit.setVisibility(View.INVISIBLE);
+            switch (v.getId()){
+                case R.id.btn_dog:
+                    linearDog.setVisibility(View.VISIBLE);
+                    break;
+                case R.id.btn_cat:
+                    linearCat.setVisibility(View.VISIBLE);
+                    break;
+                case R.id.btn_rabbit:
+                    linearRabbit.setVisibility(View.VISIBLE);
+                    break;
+            }
+        }
+    };
 }
